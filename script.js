1d13e161e6eaa5d5c354ccac118e8f94cc432965
Stefan Maties <maties.stefan44@gmail.com>
2023-03-21 10:10:00 +0100

    V2 - Simplified UI, SFW text

--- script.js
+++ script.js
@@ -1,130 +1,55 @@
-let playerSelection;
-let stringPlayerSelection;
-let computerSelection;
-let pGiftResult;
-let cGiftResult;
-
-function playerGift() {
-  document.querySelector(".result-text").style.color = "darkgreen";
-  pGift = Math.floor(Math.random() * 10) + 1;
-  switch (pGift) {
-    case 1:
-      pGiftResult = "You get Rafaelo";
-      break;
-    case 2:
-      pGiftResult = "You get hugs";
-      break;
-    case 3:
-      pGiftResult = "You get dinner made by me";
-      break;
-    case 4:
-      pGiftResult = "You get back massage";
-      break;
-    case 5:
-      pGiftResult = "You get movie night with pizza";
-      break;
-    case 6:
-      pGiftResult = "You get date night + walk";
-      break;
-    case 7:
-      pGiftResult = "You get flowers";
-      break;
-    case 8:
-      pGiftResult = "You get ice cream";
-      break;
-    case 9:
-      pGiftResult = "You get cuddling and spooning";
-      break;
-    case 10:
-      pGiftResult = "You get me :P";
-      break;
-  }
+const player = document.getElementById('player');
+const computer = document.getElementById('computer');
+const winner = document.getElementById('winner');
+const buttons = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+const giftText = [
+  'get Rafaelo',
+  'get hugs',
+  'get breakfast in bed',
+  'get back massage',
+  'get movie night with pizza',
+  'get date night',
+  'get to choose what we do tonight',
+  'get ice cream',
+  'get cuddling and spooning',
+  'get a gift',
+];
+function gift() {
+  return giftText[Math.trunc(Math.random() * 10)];
 }
-function computerGift() {
-  document.querySelector(".result-text").style.color = "darkred";
-  cGift = Math.floor(Math.random() * 10) + 1;
-  switch (cGift) {
-    case 1:
-      cGiftResult = "I get Rafaelo";
-      break;
-    case 2:
-      cGiftResult = "I get hugs";
-      break;
-    case 3:
-      cGiftResult = "I get dinner made by you";
-      break;
-    case 4:
-      cGiftResult = "I get cartoon time between your arms";
-      break;
-    case 5:
-      cGiftResult = "I get happy time";
-      break;
-    case 6:
-      cGiftResult = "I get date night + happy time";
-      break;
-    case 7:
-      cGiftResult = "I get to see boobiez";
-      break;
-    case 8:
-      cGiftResult = "I get ice cream while you just watch";
-      break;
-    case 9:
-      cGiftResult = "I get cuddling and spooning";
-      break;
-    case 10:
-      cGiftResult = "I get you naked :P";
-      break;
-    default:
-      cGiftResult = "I get hugs and kisses";
-      break;
-  }
-}
+let playerValue = '';
+let cpuValue = '';
+let resultText;
 
-function playerResult() {
-  document.querySelector(".check").addEventListener("click", function () {
-    stringPlayerSelection = document.querySelector(".guess").value;
-    playerSelection = Number(stringPlayerSelection);
-    validator();
+const cpu = function () {
+  computer.innerText = Math.trunc(Math.random() * 10 + 1);
+};
+
+buttons.forEach((x) => {
+  const button = document.getElementById(x);
+  button.addEventListener('click', () => {
+    cpu();
+    player.innerText = button.innerText;
+    playerValue = player.innerText;
+    cpuValue = computer.innerText;
+    switcher();
+    winner.classList.remove('hidden');
+    winner.innerText = resultText;
+    setTimeout(function () {
+      winner.style.background = '#051E05';
+    }, 500);
   });
-}
-
-function computerResult() {
-  computerSelection = Math.floor(Math.random() * 10) + 1;
-  document.querySelector(".computer-selection").textContent = computerSelection;
-  checker();
-}
-
-function checker() {
-  if (playerSelection == computerSelection) {
-    playerGift();
-    document.querySelector(".result-text").textContent = pGiftResult;
-    document.querySelector(".computer-selection").textContent =
-      computerSelection;
-  } else if (playerSelection != computerSelection) {
-    computerGift();
-    document.querySelector(".result-text").textContent = cGiftResult;
-  }
-  document.querySelector(".check").textContent = "Play again!";
-}
-
-function validator() {
-  if (playerSelection < 1 || playerSelection > 10) {
-    alert("U donkey, refresh browser and enter number");
-  } else {
-    computerResult();
-  }
-}
-
-function game() {
-  playerResult();
-}
-
-document.querySelector(".start-game").addEventListener("click", function () {
-  document.querySelector(".menu-game").classList.add("hidden");
-  document.querySelector(".main-holder").classList.remove("hidden");
-  game();
-  // document.getElementById("start").disabled = true;
-  // if ((document.getElementById("start").disabled = true)) {
-  //   document.getElementById("start").style.opacity = 0;
-  // }
 });
+
+function switcher() {
+  if (playerValue < cpuValue) {
+    winner.style.backgroundColor = 'darkred';
+    resultText = `You don't ${gift()}`;
+  } else if (playerValue > cpuValue) {
+    winner.style.backgroundColor = 'darkgreen';
+    resultText = `You ${gift()}`;
+  } else if (playerValue == cpuValue) {
+    winner.style.backgroundColor = 'black';
+    resultText = 'Nobody gets anything';
+  }
+}
